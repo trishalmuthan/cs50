9530db37b84de74be55aad763d18ccd7bdb14d62
Trishal Muthan <63992667+trishalmuthan@users.noreply.github.com>
2020-07-23 20:07:20 -0400

    Create 12.sql

---
+++ ProblemSet7/movies/12.sql
@@ -0,0 +1,3 @@
+SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON stars.person_id = people.id 
+WHERE people.name = "Johnny Depp" AND movies.title IN (SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id 
+JOIN people ON stars.person_id = people.id WHERE people.name = "Helena Bonham Carter");
