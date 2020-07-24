ef2b0baec342fabb7b986185ec88b29879a3ce39
Trishal Muthan <63992667+trishalmuthan@users.noreply.github.com>
2020-07-23 20:07:44 -0400

    Create 13.sql

---
+++ ProblemSet7/movies/13.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(DISTINCT(people.name)) FROM people JOIN stars ON stars.person_id = people.id JOIN movies
+ON stars.movie_id = movies.id WHERE movies.title IN (SELECT DISTINCT(movies.title) FROM movies
+JOIN stars ON stars.movie_id = movies.id JOIN people ON people.id = stars.person_id WHERE people.name
+= "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
